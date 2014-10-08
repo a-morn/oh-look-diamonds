a3ab3210122ff439e1e570b67769877c4baa0824
a-morn <albin.morner@gmail.com>
2014-10-08 13:47:30 +0200

    Rename tracks

---
+++ HTML5Application/public_html/oh/Tracks.js
@@ -0,0 +1,81 @@
+var tracksJSON = {
+    "easy" : 
+            [ 
+                //horizontal line
+                [
+                    {"x":50,"y":0,"type":"diamond","animation":"cycle", "graphicType":"sprite"},
+                    {"x":100,"y":0,"type":"diamond","animation":"cycle", "graphicType":"sprite"},
+                    {"x":150,"y":0,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":200,"y":0,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":250,"y":0,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":300,"y":0,"type":"diamond","animation":"cycle","graphicType":"sprite"}
+                ],
+                //straight downslope
+                [
+                    {"x":0,"y":0,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":50,"y":10,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":100,"y":20,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":150,"y":30,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":200,"y":40,"type":"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":250,"y":50,"type":"diamond","animation":"cycle","graphicType":"sprite"}
+                ],
+                ///looop
+                [
+                    {"x":	62.25701405	, "y":	-26.52635236	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	125.9907573	, "y":	-51.33121883	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	269.7326906	, "y":	-96.08854442	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	350.9905274	, "y":	-216.6914816	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	418.2035123	, "y":	-287.667282	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	456.2628025	, "y":	-362.6312799	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	462.9751187	, "y":	-435.1770559	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	433.5561625	, "y":	-498.6007669	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	386.002438	, "y":	-536.4105943	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	329.1994816	, "y":	-548.3956859	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	276.2562051	, "y":	-533.2310894	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	237.8197927	, "y":	-493.157281	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	225.2364304	, "y":	-431.9219154	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	244.7922799	, "y":	-359.7600216	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	296.2755166	, "y":	-294.1601402	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	372.2007193	, "y":	-235.620014	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	469.3304597	, "y":	-186.6490658	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	590.8224944	, "y":	-140.9453239	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	657.9293147	, "y":	-127.6224837	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	724.7446863	, "y":	-112.2939771	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	791.4098309	, "y":	-95.03864139	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	856.1211905	, "y":	-76.01061641	,type:"diamond","animation":"cycle","graphicType":"sprite"},]               
+            ],
+    "medium" : 
+            [
+                ///looop w seagull
+                [
+                    {"x":	370	, "y":	-350	,type:"seagull","animation":"seagull","graphicType":"sprite"},
+                    {"x":	0	, "y":	0	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	62.25701405	, "y":	-26.52635236	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	125.9907573	, "y":	-51.33121883	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	269.7326906	, "y":	-96.08854442	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	350.9905274	, "y":	-216.6914816	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	418.2035123	, "y":	-287.667282	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	456.2628025	, "y":	-362.6312799	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	462.9751187	, "y":	-435.1770559	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	433.5561625	, "y":	-498.6007669	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	386.002438	, "y":	-536.4105943	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	329.1994816	, "y":	-548.3956859	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	276.2562051	, "y":	-533.2310894	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	237.8197927	, "y":	-493.157281	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	225.2364304	, "y":	-431.9219154	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	244.7922799	, "y":	-359.7600216	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	296.2755166	, "y":	-294.1601402	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	372.2007193	, "y":	-235.620014	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	469.3304597	, "y":	-186.6490658	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	590.8224944	, "y":	-140.9453239	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	657.9293147	, "y":	-127.6224837	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	724.7446863	, "y":	-112.2939771	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	791.4098309	, "y":	-95.03864139	,type:"diamond","animation":"cycle","graphicType":"sprite"},
+                    {"x":	856.1211905	, "y":	-76.01061641	,type:"diamond","animation":"cycle","graphicType":"sprite"},]     
+                
+            ],
+    "hard" : 
+            [
+                
+            ]
+};
