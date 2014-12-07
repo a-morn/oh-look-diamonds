220d57966a0990badf2a766b6ee40aec772b91e0
rasmusoh <rasmusoh@kth.se>
2014-12-07 20:05:39 +0100

    levels

--- HTML5Application/public_html/oh/Tracks.js
+++ HTML5Application/public_html/oh/Tracks.js
@@ -1,28 +1,73 @@
 
 var tracksJSON = 
     [[
-        [{"difficulty":"easy", "name":"smallDiamond"},
-        {"difficulty":"hard", "name":"quadDucks"}],
+        //level 1
+        [{"difficulty":"easy", "name":"smallDiamond"}],
         
-        [{"difficulty":"easy", "name":"horizontalLine"},
-        {"difficulty":"easy", "name":"mediumDiamond"},
-        {"difficulty":"easy", "name":"mediumDiamond"}],
+        //level 2
+        [
+        {"difficulty":"easy", "name":"horizontalLine"},
+        {"difficulty":"easy", "name":"downSlope"},
+        {"difficulty":"easy", "name":"horizontalLine"},
+        {"difficulty":"hard", "name":"seagull"}
+        ],
         
-        [{"difficulty":"easy", "name":"loop"}],
+        //level 3
+        [
+            {"difficulty":"hard", "name":"dualDucks"},
+            {"difficulty":"easy", "name":"horizontalLine"},
+            {"difficulty":"easy", "name":"upSlope"},
+            {"difficulty":"easy", "name":"loop"}
+        ],
         
-        [{"difficulty":"hard", "name":"seagull"}],
-        
-        [{"difficulty":"easy", "name":"loop"},
-            {"difficulty":"easy", "name":"downSlope"},
+        //level 4
+        [
+            {"difficulty":"easy", "name":"sineCurve"},
+            {"difficulty":"easy", "name":"mediumDiamond"},
+            {"difficulty":"easy", "name":"sineCurve"},
+            {"difficulty":"easy", "name":"mediumDiamond"},
             {"difficulty":"hard", "name":"falcon"}],
         
-        [{"difficulty":"easy", "name":"horizontalLine"},
-            {"difficulty":"hard", "name":"duck"},
+        //level 5 
+        [{"difficulty":"easy", "name":"loop"},
+        {"difficulty":"easy", "name":"horizontalLine"},
+        {"difficulty":"easy", "name":"loop"},
+            {"difficulty":"hard", "name":"dualCrows"}],
+        
+        //level 6 
+        [
+            {"difficulty":"easy", "name":"upSlope"},
             {"difficulty":"easy", "name":"downSlope"},
-            {"difficulty":"easy", "name":"horizontalLine"},
-            {"difficulty":"easy", "name":"horizontalLine"},
-            {"difficulty":"easy", "name":"horizontalLine"},
-            {"difficulty":"hard", "name":"crow"},
-            {"difficulty":"easy", "name":"greatDiamond"}
-        ]
+            {"difficulty":"easy", "name":"zigZag"},
+            {"difficulty":"easy", "name":"loop"},
+            {"difficulty":"easy", "name":"bigDiamond"},
+            {"difficulty":"hard", "name":"quadDucks"}],
+        
+        //level 7
+        [
+            {"difficulty":"easy", "name":"downParable"},
+            {"difficulty":"easy", "name":"cloud"},
+            {"difficulty":"easy", "name":"cloud"},
+            {"difficulty":"easy", "name":"cloud"},
+            {"difficulty":"easy", "name":"cloud"},
+            {"difficulty":"easy", "name":"cloud"},
+            {"difficulty":"easy", "name":"cloud"},
+            {"difficulty":"hard", "name":"tripleBats"}
+        ],
+        
+        //level 8
+        [{"difficulty":"easy", "name":"bigSine"},
+            {"difficulty":"easy", "name":"loop"},
+            {"difficulty":"easy", "name":"loop"},
+            {"difficulty":"easy", "name":"bigDiamond"},
+            {"difficulty":"hard", "name":"quadDucks"}],
+        
+        //level 9 
+        [   
+            {"difficulty":"easy", "name":"sineCurve"},
+            {"difficulty":"easy", "name":"sineCurve"},
+            {"difficulty":"easy", "name":"sineCurve"},
+            {"difficulty":"easy", "name":"sineCurve"},
+            {"difficulty":"easy", "name":"bigDiamond"},
+            {"difficulty":"hard", "name":"glasses"}]
     ]];
