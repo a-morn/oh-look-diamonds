3d2585a849ddab18065a95b828ddaea163690db2
rasmusoh <rasmusoh@kth.se>
2015-05-25 20:28:30 +0200

    level editor fix

--- HTML5Application/public_html/oh/js/level-manifest.js
+++ HTML5Application/public_html/oh/js/level-manifest.js
@@ -1,88 +1,88 @@
 var LevelManifest = [{
 	id: "enemybirds",
-	src: "assets/new assets/sprites/newBirds.png"
+	src: "assets/sprites/newBirds.png"
 }, {
 	id: "rocketCatz",
-	src: "assets/new assets/sprites/catzOnly.png"
+	src: "assets/sprites/catzOnly.png"
 }, {
 	id: "rocket",
-	src: "assets/new assets/img/rocket.png"
+	src: "assets/img/rocket.png"
 }, {
 	id: "diamond",
-	src: "assets/new assets/sprites/newDiamond3.png"
+	src: "assets/sprites/newDiamond3.png"
 }, {
 	id: "mediumDiamond",
-	src: "assets/new assets/sprites/newDiamond2.png"
+	src: "assets/sprites/newDiamond2.png"
 }, {
 	id: "greatDiamond",
-	src: "assets/new assets/sprites/newDiamond.png"
+	src: "assets/sprites/newDiamond.png"
 }, {
 	id: "diamondShardCounter",
-	src: "assets/new assets/img/DiamondIcon.png"
+	src: "assets/img/DiamondIcon.png"
 }, {
 	id: "bg",
-	src: "assets/new assets/img/background long.jpg"
+	src: "assets/img/background long.jpg"
 }, {
 	id: "bgParallax 0",
-	src: "assets/new assets/img/background parallax.png"
+	src: "assets/img/background parallax.png"
 }, {
 	id: "bgParallax 2",
-	src: "assets/new assets/img/background parallax 4.png"
+	src: "assets/img/background parallax 4.png"
 }, {
 	id: "bgParallax 1",
-	src: "assets/new assets/img/background parallax 3.png"
+	src: "assets/img/background parallax 3.png"
 }, {
 	id: "onlookers",
-	src: "assets/new assets/sprites/onlookers.png"
+	src: "assets/sprites/onlookers.png"
 }, {
 	id: "cloud1",
-	src: "assets/new assets/img/cloud 1.png"
+	src: "assets/img/cloud 1.png"
 }, {
 	id: "cloud2",
-	src: "assets/new assets/img/cloud 2.png"
+	src: "assets/img/cloud 2.png"
 }, {
 	id: "cloud3",
-	src: "assets/new assets/img/cloud 3.png"
+	src: "assets/img/cloud 3.png"
 }, {
 	id: "cloud4",
-	src: "assets/new assets/img/cloud 4.png"
+	src: "assets/img/cloud 4.png"
 }, {
 	id: "cloud5",
-	src: "assets/new assets/img/cloud 5.png"
+	src: "assets/img/cloud 5.png"
 }, {
 	id: "hud",
-	src: "assets/new assets/img/HUD.png"
+	src: "assets/img/HUD.png"
 }, {
 	id: "hudPointer",
-	src: "assets/new assets/img/HUDpointer.png"
+	src: "assets/img/HUDpointer.png"
 }, {
 	id: "hudGlass",
-	src: "assets/new assets/sprites/hudGlass.png"
+	src: "assets/sprites/hudGlass.png"
 }, {
 	id: "mobHill1",
-	src: "assets/new assets/img/mob hill.png"
+	src: "assets/img/mob hill.png"
 }, {
 	id: "mobHill2",
-	src: "assets/new assets/img/mob hill 2.png"
+	src: "assets/img/mob hill 2.png"
 }, {
 	id: "fgGround",
-	src: "assets/new assets/img/fgGround.png"
+	src: "assets/img/fgGround.png"
 }, {
 	id: "fgGroundTop",
-	src: "assets/new assets/img/fgGroundTop.png"
+	src: "assets/img/fgGroundTop.png"
 }, {
 	id: "fgTree1",
-	src: "assets/new assets/img/tree 4.png"
+	src: "assets/img/tree 4.png"
 }, {
 	id: "rocketCatz",
-	src: "assets/new assets/sprites/catzOnly.png"
+	src: "assets/sprites/catzOnly.png"
 }, {
 	id: "rocket",
-	src: "assets/new assets/img/rocket.png"
+	src: "assets/img/rocket.png"
 }, {
 	id: "flame",
-	src: "assets/new assets/sprites/newFlame.png"
+	src: "assets/sprites/newFlame.png"
 }, {
 	id: "star",
-	src: "assets/new assets/img/star.png"
+	src: "assets/img/star.png"
 }];
