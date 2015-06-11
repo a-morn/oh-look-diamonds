6d468bf45effca7c7eddcd9738b2b37b5fe060c2
A. Mörner <albin.morner@gmail.com>
2015-06-11 21:53:51 +0200

    Add minor highscore adjustment

--- oh/js/application-control.js
+++ oh/js/application-control.js
@@ -111,22 +111,22 @@ function paint(hs){
 var dashLen = 220, dashOffset = dashLen, speed = 5,
     txt = hs+"", x = 0, i = 0;	
 ctx.clearRect(0, 0, 200, 57);
-ctx.font = "15px Arial, cursive"; 
+ctx.font = "20px Sans Serif, cursive"; 
 ctx.lineWidth = 1; ctx.lineJoin = "round"; 
 ctx.globalAlpha = 1;
 ctx.strokeStyle = "#ffffcc";
 ctx.fillStyle =	"#ffffcc";
-ctx.fillText('Most diamonds', 0, 20);
-ctx.font = "30px Just Another Hand, cursive"; 
+ctx.fillText('Highscore', 0, 20);
+ctx.font = "40px Just Another Hand, cursive"; 
 (function loop() {
-  ctx.clearRect(x, 20, 60, 40);
+  ctx.clearRect(x, 30, 60, 40);
   ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
   dashOffset -= speed;                                         // reduce dash length
-  ctx.strokeText(txt[i], x, 50);                              // stroke letter
+  ctx.strokeText(txt[i], x, 55);                              // stroke letter
 
   if (dashOffset > 0) requestAnimationFrame(loop);             // animate
   else {    
-	ctx.fillText(txt[i], x, 50);                              // fill final letter
+	ctx.fillText(txt[i], x, 55);                              // fill final letter
     dashOffset = dashLen;                                      // prep next char
     x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random() + 5;
     if (i < txt.length) requestAnimationFrame(loop);
