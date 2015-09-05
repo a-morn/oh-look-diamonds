d4e087ed4a908fa7988f34039cd3b2395011f6cf
Albin <albin@bigspin.se>
2015-09-05 22:51:32 +0200

    add angular

--- src/js/application-control.js
+++ src/js/application-control.js
@@ -1,6 +1,6 @@
-var 
-	bg,            
-	canvas, 
+var
+	bg,
+	canvas,
 	catzBounds,
 	catzNorm,        
 	catzVertices,
@@ -48,19 +48,19 @@ var
 		lightning : new createjs.Container(),
 		onlooker : new createjs.Container(),
 		parallax : new createjs.Container(),
-		//scatterDiamonds : new createjs.Container(),		
+		//scatterDiamonds : new createjs.Container(),
 		sg : new createjs.Container(),
-		star : new createjs.Container(),	
+		star : new createjs.Container(),
 		thunder : new createjs.Container(),
-		wind : new createjs.Container()},	
+		wind : new createjs.Container()},
 	gameStats = {
         score : 0,
         kills : 0,
         bust : 0,
-        currentRound: 0,        
+        currentRound: 0,
 		buildings: {
 			hoboCatHouse : {built : false, builtOnRound : null} ,
-			orphanage : {built : false, builtOnRound : null, youthCenter : false, summerCamp : false, slots : 0},       
+			orphanage : {built : false, builtOnRound : null, youthCenter : false, summerCamp : false, slots : 0},
 			rehab: {built : false, isBuilding : false, builtOnRound : null , hospital : false, phychiatricWing : false, monastery : false, slots : 0},        
 			university: {built : false, builtOnRound : null, rocketUniversity:null, slots : 0}
 		},
@@ -81,16 +81,18 @@ var
         Difficulty : 0,
         hasBeenFirst: {
             round : false,
-            frenzy : false,            
+            frenzy : false,
             houseWithSlots : false,
             bouncedCheck : false
-        }        
-    };	
-	
-function StartGame(){    	    
+        }
+    };
+
+console.log('app-con');
+
+function StartGame(){
 	$("#mute").click(switchMute);
 	ctx = document.querySelector("#hs").getContext("2d"),
-	InitializeStage.init(canvas, stage);    
+	InitializeStage.init(canvas, stage);
 	Cookie.saveAndSetHS(0);
 }
 
@@ -101,32 +103,31 @@ function switchMute(){
 	}
 	else{
 		createjs.Sound.setMute(true);
-		$("#mute").toggleClass("mute-is-muted",true);    
+		$("#mute").toggleClass("mute-is-muted",true);
 	}
 }
 
-$(StartGame);
-
 function paint(hs){
 var dashLen = 220, dashOffset = dashLen, speed = 5,
-    txt = hs+"", x = 0, i = 0;	
+    txt = hs+"", x = 0, i = 0;
 ctx.clearRect(0, 0, 200, 57);
-ctx.font = "20px Sans Serif, cursive"; 
-ctx.lineWidth = 1; ctx.lineJoin = "round"; 
+ctx.font = "20px Sans Serif, cursive";
+ctx.lineWidth = 1; ctx.lineJoin = "round";
 ctx.globalAlpha = 1;
 ctx.strokeStyle = "#ffffcc";
 ctx.fillStyle =	"#ffffcc";
 ctx.fillText('Highscore', 0, 20);
-ctx.font = "40px Just Another Hand, cursive"; 
+ctx.font = "40px Just Another Hand, cursive";
 (function loop() {
   ctx.clearRect(x, 30, 60, 40);
   ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
   dashOffset -= speed;                                         // reduce dash length
   ctx.strokeText(txt[i], x, 55);                              // stroke letter
 
-  if (dashOffset > 0) requestAnimationFrame(loop);             // animate
-  else {    
-	ctx.fillText(txt[i], x, 55);                              // fill final letter
+  if (dashOffset > 0)
+    requestAnimationFrame(loop);             // animate
+  else {
+    ctx.fillText(txt[i], x, 55);                              // fill final letter
     dashOffset = dashLen;                                      // prep next char
     x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random() + 5;
     if (i < txt.length) requestAnimationFrame(loop);
